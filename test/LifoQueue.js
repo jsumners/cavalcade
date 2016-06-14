8be0fcd011dba96700db9ca065285dcce3df2ad8
James Sumners <james.sumners@gmail.com>
2016-06-14 16:07:39 -0400

    Initial commit and release

---
+++ test/LifoQueue.js
@@ -0,0 +1,144 @@
+'use strict'
+/* eslint-env node, mocha */
+
+const path = require('path')
+const expect = require('chai').expect
+const lifoQueueFactory = require(path.join(__dirname, '..', 'lib', 'LifoQueue'))
+
+suite('LifoQueue', function () {
+  suite('#ctor(size, initial)', function () {
+    test('creates queues with a default size of 16', function (done) {
+      const q = lifoQueueFactory()
+      expect(q.size).to.equal(16)
+      done()
+    })
+
+    test('creates queues with a specified size', function (done) {
+      const q = lifoQueueFactory(2)
+      expect(q.size).to.equal(2)
+      done()
+    })
+
+    test('creates queues with a specified size and initial values', function (done) {
+      const q = lifoQueueFactory(2, [1, 2])
+      expect(q.size).to.equal(2)
+      expect(q.length).to.equal(2)
+      expect(q.pop()).to.equal(2)
+      expect(q.pop()).to.equal(1)
+      done()
+    })
+
+    test('will not create a queue with an invalid size', function (done) {
+      expect(lifoQueueFactory.bind(null, 1.2)).to.throw(Error)
+      done()
+    })
+
+    test('will not create a queue with an invalid initial value', function (done) {
+      expect(lifoQueueFactory.bind(null, 1, 1)).to.throw(Error)
+      done()
+    })
+  })
+
+  suite('#length', function () {
+    test('returns the number of pushed elements', function (done) {
+      const q = lifoQueueFactory(2)
+      q.push(1)
+      expect(q.length).to.equal(1)
+      q.push(2)
+      expect(q.length).to.equal(2)
+      done()
+    })
+  })
+
+  suite('#size', function () {
+    test('returns the number of slots in the queue', function (done) {
+      const q = lifoQueueFactory(2)
+      q.push(1)
+      expect(q.size).to.equal(2)
+      q.push(2)
+      expect(q.size).to.equal(2)
+      done()
+    })
+  })
+
+  suite('#push(item)', function () {
+    test('adds to the beginning of the queue', function (done) {
+      let q = lifoQueueFactory(2)
+      q.push(1)
+
+      let values = q.values()
+      expect(values.next().value).to.equal(1)
+      expect(values.next().value).to.be.undefined
+
+      q = lifoQueueFactory(4)
+      q.push(1)
+      q.push(2)
+      q.push(3)
+      q.push(4)
+      values = q.values()
+      expect(values.next().value).to.equal(4)
+      expect(values.next().value).to.equal(3)
+      expect(values.next().value).to.equal(2)
+      expect(values.next().value).to.equal(1)
+      q.push(5)
+      values = q.values()
+      expect(values.next().value).to.equal(5)
+      expect(values.next().value).to.equal(4)
+      expect(values.next().value).to.equal(3)
+      expect(values.next().value).to.equal(2)
+      expect(values.next().value).to.be.undefined
+
+      done()
+    })
+
+    test('returns the number of elements in the queue', function (done) {
+      let q = lifoQueueFactory()
+      expect(q.length).to.equal(0)
+      expect(q.push(1)).to.equal(1)
+      expect(q.length).to.equal(1)
+      done()
+    })
+  })
+
+  suite('#pop()', function () {
+    test('dequeues elements from the front of the queue', function (done) {
+      const q = lifoQueueFactory(4)
+      expect(q.pop()).to.be.null
+
+      q.push(1)
+      q.push(2)
+      q.push(3)
+      q.push(4)
+
+      expect(q.pop()).to.equal(4)
+      expect(q.pop()).to.equal(3)
+      expect(q.pop()).to.equal(2)
+      expect(q.pop()).to.equal(1)
+      expect(q.length).to.equal(0)
+      expect(q.pop()).to.equal(null)
+
+      done()
+    })
+  })
+
+  suite('#values()', function () {
+    test('does not modify the queue', function (done) {
+      const q = lifoQueueFactory(4, [1, 2, 3, 4])
+      const values = q.values()
+
+      expect(values.next().value).to.equal(4)
+      expect(q.length).to.equal(4)
+
+      expect(values.next().value).to.equal(3)
+      expect(q.length).to.equal(4)
+
+      expect(values.next().value).to.equal(2)
+      expect(q.length).to.equal(4)
+
+      expect(values.next().value).to.equal(1)
+      expect(q.length).to.equal(4)
+
+      done()
+    })
+  })
+})
